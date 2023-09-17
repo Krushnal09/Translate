e744cd8f57d2648f55df6dd60ba408a1a7835a74
Krushnal <krushnalpatil2018@gmail.com>
2023-09-17 23:15:56 +0530

    11

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from googletrans import Translator
+
+app = Flask(__name__)
+
+def translate_text(text, target_language):
+    translator = Translator()
+    translated_text = translator.translate(text, src='en', dest=target_language)
+    return translated_text.text
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    translation = None
+    error = None
+
+    if request.method == 'POST':
+        text = request.form.get('text', '')
+        target_language = request.form.get('language', 'hi')  # Default to Hindi if not selected
+        if text.strip():
+            translation = translate_text(text, target_language)
+        else:
+            error = "Please enter text to translate."
+
+    return render_template('index.html', translation=translation, error=error)
+
+if __name__ == "__main__":
+    app.run(debug=True)
